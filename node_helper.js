dabd007c4b194517998b1947ebb3bb5bcec8b056
Nikolai Nordby <46522217+nmeldre@users.noreply.github.com>
2026-03-25 15:08:04 +0100

    Implement NodeHelper for pollen data updates

---
+++ node_helper.js
@@ -0,0 +1,51 @@
+const NodeHelper = require("node_helper");
+const request = require("request");
+const fs = require("fs");
+const path = require("path");
+
+module.exports = NodeHelper.create({
+    start: function() {
+        this.historyPath = path.join(__dirname, "pollen-history.json");
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === "CONFIG") {
+            this.config = payload;
+            this.updateData();
+            setInterval(() => this.updateData(), this.config.updateInterval);
+        }
+    },
+
+    updateData: function() {
+        const url = `https://pollen.googleapis.com/v1/forecast:lookup?key=${this.config.apiKey}&location.latitude=${this.config.latitude}&location.longitude=${this.config.longitude}&days=5&languageCode=${this.config.language}`;
+        
+        request({ url: url, json: true }, (err, res, body) => {
+            if (err || (res && res.statusCode !== 200)) return;
+
+            let history = this.getHistory();
+            const today = new Date().toISOString().split('T')[0];
+            
+            if (body.dailyInfo && body.dailyInfo[0]) {
+                history[today] = body.dailyInfo[0].plantInfo.map(p => ({
+                    code: p.code,
+                    value: p.indexInfo ? p.indexInfo.value : 0,
+                    color: p.indexInfo ? p.indexInfo.color : null
+                }));
+
+                const keys = Object.keys(history).sort().slice(-10);
+                let newHistory = {};
+                keys.forEach(k => newHistory[k] = history[k]);
+                
+                fs.writeFileSync(this.historyPath, JSON.stringify(newHistory));
+                this.sendSocketNotification("DATA_UPDATE", { forecast: body.dailyInfo, history: newHistory });
+            }
+        });
+    },
+
+    getHistory: function() {
+        if (fs.existsSync(this.historyPath)) {
+            try { return JSON.parse(fs.readFileSync(this.historyPath, "utf8")); } catch (e) { return {}; }
+        }
+        return {};
+    }
+});
